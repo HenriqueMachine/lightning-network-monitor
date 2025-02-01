3171c6198780bce8b3f84690434c192313b4372c
Henrique Menezes <henrique.devmobile@gmail.com>
2025-01-31 21:48:47 -0300

    creating app module

---
+++ app/src/main/java/com/henriquemachine/lightningmonitornetwork/di/AppModule.kt
@@ -0,0 +1,52 @@
+package com.henriquemachine.lightningmonitornetwork.di
+
+import com.henriquemachine.lightningmonitornetwork.BuildConfig
+import com.henriquemachine.lightningmonitornetwork.data.api.LightningApi
+import com.henriquemachine.lightningmonitornetwork.data.repository.LightningRepository
+import com.henriquemachine.lightningmonitornetwork.data.repository.NodeRepositoryImpl
+import com.henriquemachine.lightningmonitornetwork.domain.GetLightningNodesUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.API_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): LightningApi {
+        return retrofit.create(LightningApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNodeRepository(apiService: LightningApi): LightningRepository {
+        return NodeRepositoryImpl(apiService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetLightningNodesUseCase(repository: LightningRepository): GetLightningNodesUseCase {
+        return GetLightningNodesUseCase(repository)
+    }
+}
